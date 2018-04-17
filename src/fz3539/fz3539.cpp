1fe5568f83aeb309017d49b1f9420a85c1db5f63
Alex <ajmanor@wayne.edu>
2018-04-17 16:39:18 -0400

    Added comments and final touches

--- src/fz3539/fz3539.cpp
+++ src/fz3539/fz3539.cpp
@@ -1,6 +1,6 @@
 #include <openssl/rc4.h>
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h> // For pread and pwrite
 
 #include "fz3539.h"
 
@@ -9,23 +9,23 @@ using namespace std;
 void fz3539RC4(int fd) {
 	size_t bufferSize = lseek(fd, 0, SEEK_END);
 
-	if (bufferSize < 0)
+	if (bufferSize == -1) // Error according to man7.org/linux/man-pages/man2/lseek.2.html
 		exit(EXIT_FAILURE);
-
-	lseek(fd, 0, SEEK_SET); // reset 
 
 	unsigned char input_buffer[bufferSize];
 	unsigned char *output_buffer = (unsigned char*)malloc(bufferSize+1);
+	
+	lseek(fd, 0, SEEK_SET); // reset 
 
 	if (pread(fd, &input_buffer, bufferSize, 0) == -1)
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); // Error according to man7.org/linux/man-pages/man2/pread.2.html
 	
 	RC4_KEY key;
 	RC4_set_key(&key, len, (const unsigned char *)data);
 	RC4(&key, bufferSize, input_buffer, output_buffer);
 
 	if (pwrite(fd, output_buffer, bufferSize, 0) == -1)
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); // Error
 
 	free(output_buffer);
 }
